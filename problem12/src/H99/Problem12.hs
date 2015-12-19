b5577f15173e3762e289ed14da533273ac7f25aa
shimanekb <brandon.j.shimanek@gmail.com>
2015-12-19 12:28:42 -0600

    Refined solution

--- problem12/src/H99/Problem12.hs
+++ problem12/src/H99/Problem12.hs
@@ -6,7 +6,6 @@ data Count b = Single b |
                deriving (Show, Eq, Read)
 
 decode :: Eq a => [Count a] -> [a]
-decode []     = []
-decode (x:xs) = (decodeHelper x) ++  (decode xs)
-                where decodeHelper (Single y) = [y]
-                      decodeHelper (Multiple n y) = replicate n y
+decode = foldr (\x z -> decodeHelper x ++ z) []
+         where decodeHelper (Single y) = [y]
+               decodeHelper (Multiple n y) = replicate n y
