5c6fce1ff81346dea1e78451182c82b2c3c78f50
shimanekb <brandon.j.shimanek@gmail.com>
2015-12-28 15:55:26 -0600

    Finished problem19

--- problem19/src/H99/Problem19.hs
+++ problem19/src/H99/Problem19.hs
@@ -3,4 +3,6 @@ module H99.Problem19
 
 rotate :: [a] -> Int -> [a]
 rotate [] _ = []
-rotate xs n = xs
+rotate xs n 
+  | n < 0     = drop (length xs + n) xs ++ take (length xs + n) xs
+  | otherwise = drop n xs ++ take n xs
--- problem19/src/Main.hs
+++ problem19/src/Main.hs
@@ -1,1 +1,7 @@
-main = do putStrLn "Hello"
+import H99.Problem19
+
+main = do putStrLn "Enter String: "
+          inputList <- readLn :: IO [Char]
+          putStrLn "Enter n: "
+          n <- readLn :: IO (Int)
+          putStrLn $ show $ rotate inputList n
